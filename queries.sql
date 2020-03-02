ca098c887eab39775bebd7c1872f6096e807303d
unknown <cjradomski@gmail.com>
2020-03-01 18:22:08 -0800

    Adding schema for creating tables in pgAdmin and initial queries

---
+++ queries.sql
@@ -0,0 +1,7 @@
+-- Querying with SELECT statement to check
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM employees;
+SELECT * FROM titles;
+SELECT * FROM salaries;
+SELECT * FROM dept_manager;
