1688e92cf8163600c9eae044bfce48d58c1240c4
unknown <cjradomski@gmail.com>
2020-03-01 19:29:36 -0800

    Adding new table with employees about to retire

--- queries.sql
+++ queries.sql
@@ -5,3 +5,45 @@ SELECT * FROM employees;
 SELECT * FROM titles;
 SELECT * FROM salaries;
 SELECT * FROM dept_manager;
+
+-- Query to determine Retirement Eligibility (years 1952-1955)
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Query to COUNT Retirement Eligibility (years 1952-1955)
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Query to determine Retirement Eligibility (1952 only)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Query to determine Retirement Eligibility (1953 only)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- Query to determine Retirement Eligibility (1954 only)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- Query to determine Retirement Eligibility (1955 only)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Query to determine Retirement Eligibility (years 1952-1955) and save to new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Query to check new retirement table
+SELECT * FROM retirement_info
